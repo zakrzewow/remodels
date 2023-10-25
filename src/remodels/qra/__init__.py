0903ee57fbdd5beacf3f6e20e9171edf47da6a53
Grzegorz Zakrzewski <zakrzew12@gmail.com>
2023-10-25 10:26:29 +0200

    [ADD] LQRA, SQRA

--- src/remodels/qra/__init__.py
+++ src/remodels/qra/__init__.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy.optimize import linprog as scipy_linprog
 from scipy.optimize import minimize
+from scipy.stats import iqr
 from scipy.stats import norm
 
 
@@ -45,10 +46,19 @@ def _lqra(X, y, quantile: float, lambda_: float = 0.0, fit_intercept: bool = Fal
 
 def _sqra(X, y, quantile: float, H: float, fit_intercept=False):
     # SQRA can be solved with gradient-descent method (2022-he)
+    # for now, TNC (non-linear) minimizer is used
 
     if fit_intercept:
         N = X.shape[0]
         X = np.concatenate([np.ones((N, 1)), X], axis=1)
+
+    qra = QRA(quantile=quantile, fit_intercept=False).fit(X, y)
+    beta__initial_guess = qra._beta
+    if H is None:  # rule-of-thumb estimation
+        residuals = y - qra.predict(X)
+        resid_std = np.std(residuals)
+        resid_iqr = iqr(residuals)
+        H = min(resid_std, resid_iqr / 1.38898) * 1.06 * (X.shape[0] ** (-1 / 5))
 
     def rho(beta):
         residuals = y - X @ beta
@@ -65,24 +75,24 @@ class QRA:
     """QRA."""
 
     def __init__(self, quantile: float, fit_intercept: bool = False) -> None:
-        """Initializes QRA model.
+        """Initialize QRA model.
 
         :param quantile: quantile
         :type quantile: float
-        :param fit_intercept: True if fit intercept in QRA model, defaults to False
+        :param fit_intercept: True if fit intercept in model, defaults to False
         :type fit_intercept: bool, optional
         """
         self.quantile = quantile
         self.fit_intercept = fit_intercept
 
     def fit(self, X: np.array, y: np.array) -> "QRA":
-        """Fits QRA model.
+        """Fit model.
 
         :param X: input matrix
         :type X: np.array
         :param y: dependent variable
         :type y: np.array
-        :return: fitted QRA model
+        :return: fitted model
         :rtype: QRA
         """
         beta = _lqra(X, y, self.quantile, 0, self.fit_intercept)
@@ -90,6 +100,7 @@ class QRA:
         return self
 
     def _assign_coef_and_intercept(self, beta: np.array):
+        self._beta = beta
         if self.fit_intercept:
             self._coef = beta[1:]
             self._intercept = beta[0]
@@ -98,7 +109,7 @@ class QRA:
             self._intercept = 0
 
     def predict(self, X: np.array) -> np.array:
-        """Predicts dependent variable.
+        """Predict dependent variable.
 
         :param X: input matrix
         :type X: np.array
@@ -112,30 +123,149 @@ class QRM(QRA):
     """QRM."""
 
     def __init__(self, quantile: float, fit_intercept: bool = False) -> None:
-        """Initializes QRM model.
+        """Initialize QRM model.
 
         :param quantile: quantile
         :type quantile: float
-        :param fit_intercept: True if fit intercept in QRA model, defaults to False
+        :param fit_intercept: True if fit intercept in model, defaults to False
         :type fit_intercept: bool, optional
         """
         super().__init__(quantile=quantile, fit_intercept=fit_intercept)
 
     def fit(self, X: np.array, y: np.array) -> "QRM":
-        """Fits QRM model.
+        """Fit model.
 
         :param X: input matrix
         :type X: np.array
         :param y: dependent variable
         :type y: np.array
-        :return: fitted QRM model
+        :return: fitted model
         :rtype: QRM
         """
         X = np.mean(X, axis=1, keepdims=True)
         return super().fit(X, y)
 
     def predict(self, X: np.array) -> np.array:
-        """Predicts dependent variable.
+        """Predict dependent variable.
+
+        :param X: input matrix
+        :type X: np.array
+        :return: prediction
+        :rtype: np.array
+        """
+        X = np.mean(X, axis=1, keepdims=True)
+        return super().predict(X)
+
+
+class LQRA(QRA):
+    """LQRA."""
+
+    def __init__(
+        self,
+        quantile: float,
+        lambda_: float = 0.0,
+        fit_intercept: bool = False,
+    ) -> None:
+        """Initialize LQRA model.
+
+        :param quantile: quantile
+        :type quantile: float
+        :param fit_intercept: True if fit intercept in model, defaults to False
+        :type fit_intercept: bool, optional
+        :param lambda_: LASSO regularization parameter
+        :type lambda_: float
+        """
+        super().__init__(quantile=quantile, fit_intercept=fit_intercept)
+        self.lambda_ = lambda_
+
+    def fit(self, X: np.array, y: np.array) -> "LQRA":
+        """Fit model.
+
+        :param X: input matrix
+        :type X: np.array
+        :param y: dependent variable
+        :type y: np.array
+        :return: fitted model
+        :rtype: LQRA
+        """
+        beta = _lqra(X, y, self.quantile, self.lambda_, self.fit_intercept)
+        self._assign_coef_and_intercept(beta)
+        return self
+
+
+class SQRA(QRA):
+    """SQRA."""
+
+    def __init__(
+        self,
+        quantile: float,
+        H: float = None,
+        fit_intercept: bool = False,
+    ) -> None:
+        """Initialize SQRA model.
+
+        :param quantile: quantile
+        :type quantile: float
+        :param fit_intercept: True if fit intercept in model, defaults to False
+        :type fit_intercept: bool, optional
+        :param H: smoothing parameter called the bandwidth, must be positive
+            real number; if None, it is automatically estimated using Scott's
+            (or Silverman's) rule-of-thumb
+        :type H: float
+        """
+        super().__init__(quantile=quantile, fit_intercept=fit_intercept)
+        self.H = H
+
+    def fit(self, X: np.array, y: np.array) -> "SQRA":
+        """Fit model.
+
+        :param X: input matrix
+        :type X: np.array
+        :param y: dependent variable
+        :type y: np.array
+        :return: fitted model
+        :rtype: SQRA
+        """
+        beta = _sqra(X, y, self.quantile, self.H, self.fit_intercept)
+        self._assign_coef_and_intercept(beta)
+        return self
+
+
+class SQRM(SQRA):
+    """SQRM."""
+
+    def __init__(
+        self,
+        quantile: float,
+        H: float = 0.0,
+        fit_intercept: bool = False,
+    ) -> None:
+        """Initialize SQRM model.
+
+        :param quantile: quantile
+        :type quantile: float
+        :param fit_intercept: True if fit intercept in model, defaults to False
+        :type fit_intercept: bool, optional
+        :param H: smoothing parameter called the bandwidth
+        :type H: float
+        """
+        super().__init__(quantile=quantile, H=H, fit_intercept=fit_intercept)
+
+    def fit(self, X: np.array, y: np.array) -> "SQRM":
+        """Fit model.
+
+        :param X: input matrix
+        :type X: np.array
+        :param y: dependent variable
+        :type y: np.array
+        :return: fitted model
+        :rtype: SQRM
+        """
+        X = np.mean(X, axis=1, keepdims=True)
+        return super().fit(X, y)
+
+    def predict(self, X: np.array) -> np.array:
+        """Predict dependent variable.
 
         :param X: input matrix
         :type X: np.array
--- tests/qra/test_init.py
+++ tests/qra/test_init.py
@@ -3,9 +3,13 @@ from typing import Tuple
 
 import numpy as np
 import pytest
+from sklearn.linear_model import QuantileRegressor
 
+from remodels.qra import LQRA
 from remodels.qra import QRA
 from remodels.qra import QRM
+from remodels.qra import SQRA
+from remodels.qra import SQRM
 
 
 @pytest.fixture
@@ -18,22 +22,66 @@ def sample_data() -> Tuple[np.array, np.array]:
 
 
 def test_qra_predicitons(sample_data: Tuple[np.array, np.array]) -> None:
-    """Sum of QRA model predictions equals specific number."""
+    """QRA model predictions are equal to sklearn QR implementation predictions."""
     X_, y_ = sample_data
-    assert QRA(0.5, False).fit(X_, y_).predict(X_).sum() == pytest.approx(
-        982.0416688210856
+
+    qra_predictions = QRA(0.5, True).fit(X_, y_).predict(X_)
+    sklearn_qr_predictions = (
+        QuantileRegressor(quantile=0.5, alpha=0, fit_intercept=True, solver="highs")
+        .fit(X_, y_)
+        .predict(X_)
     )
-    assert QRA(0.5, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
-        984.5833310199318
-    )
+    assert qra_predictions == pytest.approx(sklearn_qr_predictions)
 
 
 def test_qrm_predicitons(sample_data: Tuple[np.array, np.array]) -> None:
-    """Sum of QRM model predictions equals specific number."""
+    """QRM model predictions are equal to sklearn QR implementation predictions."""
     X_, y_ = sample_data
-    assert QRM(0.5, False).fit(X_, y_).predict(X_).sum() == pytest.approx(
-        976.5882131381337
+
+    qrm_predictions = QRM(0.5, True).fit(X_, y_).predict(X_)
+
+    X_mean = np.mean(X_, axis=1, keepdims=True)
+    sklearn_qr_predictions = (
+        QuantileRegressor(quantile=0.5, alpha=0, fit_intercept=True, solver="highs")
+        .fit(X_mean, y_)
+        .predict(X_mean)
     )
-    assert QRM(0.5, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
-        979.7208019501684
+    assert qrm_predictions == pytest.approx(sklearn_qr_predictions)
+
+
+def test_lqra_predicitons(sample_data: Tuple[np.array, np.array]) -> None:
+    """LQRA model predictions are equal to sklearn QR implementation predictions."""
+    X_, y_ = sample_data
+
+    lqra_predictions = LQRA(0.5, 1, True).fit(X_, y_).predict(X_)
+    sklearn_qr_predictions = (
+        QuantileRegressor(quantile=0.5, alpha=1, fit_intercept=True, solver="highs")
+        .fit(X_, y_)
+        .predict(X_)
+    )
+    assert lqra_predictions == pytest.approx(sklearn_qr_predictions)
+
+
+def test_sqra_predicitons(sample_data: Tuple[np.array, np.array]) -> None:
+    """Sum of SQRA model predictions equals specific number."""
+    X_, y_ = sample_data
+    assert SQRA(0.5, 1, False).fit(X_, y_).predict(X_).sum() == pytest.approx(
+        989.015207045325
+    )
+    assert SQRA(0.5, 1, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
+        990.8340987938492
+    )
+    assert SQRA(0.5, None, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
+        989.6052326758037
+    )
+
+
+def test_sqrm_predicitons(sample_data: Tuple[np.array, np.array]) -> None:
+    """Sum of SQRM model predictions equals specific number."""
+    X_, y_ = sample_data
+    assert SQRM(0.5, 1, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
+        991.161454057788
+    )
+    assert SQRM(0.5, None, True).fit(X_, y_).predict(X_).sum() == pytest.approx(
+        990.1044896031061
     )
